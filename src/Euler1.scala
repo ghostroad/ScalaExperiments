419ba29892df7d343db1d99a6e39a0b221941370
Mushfeq <mushfeq.khan@gmail.com>
2018-08-29 01:39:10 -0400

    Progress

--- src/Euler1.scala
+++ src/Euler1.scala
@@ -1,8 +1,8 @@
 object Euler1 extends App {
   val range = 1 until 1000
-  val answer = range.filter(_ % 3 == 0).foldLeft(0)(_ + _) +
-    range.filter(_ % 5 == 0).foldLeft(0)(_ + _) -
-      range.filter(_ % 15==0).foldLeft(0)(_ + _)
+  val answer = range.filter(_ % 3 == 0).sum +
+    range.filter(_ % 5 == 0).sum -
+      range.filter(_ % 15 == 0).sum
   print(answer)
 
 }
--- src/Euler2.scala
+++ src/Euler2.scala
@@ -5,5 +5,5 @@ object Euler2 extends App{
 
   val fib: Stream[Int] = 0 #:: Utils.unfold((0, 1)) {case (prevprev, prev) => (prevprev + prev, (prev, prevprev + prev))}
 
-  println(fib.filter(_ % 2 == 0).takeWhile(_ < 4000000).foldLeft(0)(_ + _))
+  println(fib.filter(_ % 2 == 0).takeWhile(_ < 4000000).sum)
 }
